f4b5217eadf2b9945f0c4c13abe5707e49557495
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-10-14 20:35:54 -0700

    updated

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,139 +1,161 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
-import Button from '@mui/material/Button';
-// import API from "../../utils/API";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Constants from "../../utils/constants";
+import {
+	AppBar,
+	Box,
+	Toolbar,
+	Button,
+	IconButton,
+	Typography,
+	TextField,
+	Grid
+} from "@mui/material";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import EmailIcon from "@mui/icons-material/Email";
+import KeyIcon from "@mui/icons-material/Key";
+import PhoneIcon from "@mui/icons-material/Phone";
+import PersonIcon from "@mui/icons-material/Person";
 
-const Signup = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [phonenumber, setPhonenumber] = useState('')
-    const [name, setName] = useState('')
-    // const navigate = useNavigate();
+const Signup = props => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [phone, setPhone] = useState("");
+	const [name, setName] = useState("");
+	const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+	useEffect(() => {
+		if (!localStorage.getItem(Constants.LOCALSTORAGE_TOKEN_KEY)) {
+			navigate("");
+		} else {
+			navigate("/scheduler");
+		}
+	}, []);
 
-        const userData = {
-            email: email,
-            password: password,
-            name: name,
-        }
-        console.log(userData)
-        fetch('http://localhost:3001/api/customer/', {
-            method: 'POST',
-            body: JSON.stringify(userData),
-            headers: { 'Content-Type': 'application/json' }
-        }).then(placeHolder => {
+	const handleSubmit = async e => {
+		e.preventDefault();
 
+		const userData = {
+			email: email,
+			password: password,
+			name: name,
+			phone: phone
+		};
 
-            return placeHolder.json()
+		fetch("http://localhost:3001/api/customer/", {
+			method: "POST",
+			body: JSON.stringify(userData),
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(placeHolder => placeHolder.json())
+			.then(placeHolder => {
+				if (placeHolder.hasOwnProperty("err")) {
+					alert(placeHolder.msg);
+					return;
+				}
 
-        }).then(placeHolder => {
+				localStorage.setItem(
+					Constants.LOCALSTORAGE_TOKEN_KEY,
+					placeHolder.token
+				);
+				localStorage.setItem(
+					Constants.LOCALSTORAGE_CUSTOMER_KEY,
+					JSON.stringify(placeHolder.customer)
+				);
 
-            console.log(placeHolder)
-            localStorage.setItem('token', '')
+				props.setIsLoggedIn(true);
+				navigate("/scheduler");
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
 
-
-
-            console.log('successfully logged in')
-
-            // navigate('/scheduler');
-        }).catch((err) => { console.log(err) })
-    }
-
-    return (
-        <form className="signup" onSubmit={handleSubmit}>
-            <h3>Sign up</h3>
-
-            <label>Name:</label>
-            <input
-                type="name"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                placeholder='Customer Name'
-            />
-
-            <label>Email:</label>
-            <input
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                placeholder='Email'
-            />
-            <label>Password:</label>
-            <input
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                placeholder='Password'
-            />
-            <label>Phone Number:</label>
-            <input
-                type="phonenumber"
-                onChange={(e) => setPhonenumber(e.target.value)}
-                value={phonenumber}
-                placeholder='xxx-xxx-xxxx'
-            />
-
-            <Button variant="contained" style={{ display: "block", margin: "0 auto" }} type="submit" value="Submit" >Sign Up</Button>{''}
-        </form>
-    )
-}
-// const SignUp = ({changePage, changeUser}) => {
-//     const [username, setUsername] = useState("");
-//     const [password, setPassword] = useState("");
-//     const [checkPassword, setCheckPassword] = useState("");
-//     const [message, setMessage] = useState("");
-
-//     const isSamePassword = () => {
-//         let passMatch = password === checkPassword;
-//         setMessage(passMatch ? "" : "Passwords must match");
-//         return passMatch;
-//     }
-
-//     const handleInputChange = (e) => {
-//         switch (e.target.id){
-//             case "signUp-user":
-//                 setUsername(e.target.value);
-//                 break;
-//             case "signUp-pass":
-//                 setPassword(e.target.value);
-//                 break;
-//             case "signUp-check":
-//                 setCheckPassword(e.target.value);
-//                 break;
-//         }
-//     }
-
-//     const handleSubmit = async () => {
-//         if (username && password && isSamePassword()){
-//             if (await API.checkName(username)){
-
-//                 let userToAdd = {
-//                     name: username,
-//                     password: password
-//                 }
-
-//                 let added = await API.addUser(userToAdd);
-
-//                 if (added?.username){
-//                     changeUser(added.username, added.userId);
-//                     changePage("imageAdd");
-//                 }
-//             };
-//         }
-//     }
-
-//     return (
-//         <div>
-//             <div>{message}</div>
-//             <input type="text" id="signUp-user" onChange={handleInputChange} />
-//             <input type="password" id="signUp-pass" onChange={handleInputChange} />
-//             <input type="password" id="signUp-check" onChange={handleInputChange} />
-//             <button onClick={handleSubmit}>Sign Up</button>
-//             <div><a onClick={()=>changePage("logIn")}>Log In instead!</a></div>
-//         </div>
-//     )
-// }
+	return (
+		<form className="login" onSubmit={handleSubmit}>
+			<Grid
+				container
+				alignItems="center"
+				justify="center"
+				direction="column"
+			>
+				<Typography
+					variant="h3"
+					style={{ marginBottom: 50, marginTop: 50 }}
+				>
+					Sign up
+				</Typography>
+				<TextField
+					label="Name"
+					type="text"
+					variant="standard"
+					value={name}
+					onChange={e => setName(e.target.value)}
+					style={{ width: 400, marginBottom: 10 }}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment>
+								<PersonIcon style={{ color: "#aaa" }} />
+							</InputAdornment>
+						)
+					}}
+				/>
+				<TextField
+					label="Email"
+					type="email"
+					variant="standard"
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+					style={{ width: 400, marginBottom: 10 }}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment>
+								<EmailIcon style={{ color: "#aaa" }} />
+							</InputAdornment>
+						)
+					}}
+				/>
+				<TextField
+					label="Password"
+					type="password"
+					variant="standard"
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+					style={{ width: 400, marginBottom: 10 }}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment>
+								<KeyIcon style={{ color: "#aaa" }} />
+							</InputAdornment>
+						)
+					}}
+					helperText="Length should be between 8 to 12 characters."
+				/>
+				<TextField
+					label="Phone"
+					type="text"
+					variant="standard"
+					value={phone}
+					onChange={e => setPhone(e.target.value)}
+					style={{ width: 400, marginBottom: 10 }}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment>
+								<PhoneIcon style={{ color: "#aaa" }} />
+							</InputAdornment>
+						)
+					}}
+				/>
+				<Button
+					variant="contained"
+					style={{ width: 400, marginTop: 30, marginBottom: 50 }}
+					type="submit"
+				>
+					Sign Up
+				</Button>
+			</Grid>
+		</form>
+	);
+};
 
 export default Signup;
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const Constants = {
+	LOCALSTORAGE_TOKEN_KEY: "@ClipHub_token",
+	LOCALSTORAGE_CUSTOMER_KEY: "@ClipHub_customer"
+};
+
+export default Constants;
